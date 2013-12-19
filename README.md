292af34efb1c6b7b2f5d6b56d831a50cc4c8a1c7
yunfuliu <yunfuliu@gmail.com>
2013-12-19 11:29:11 +0800

    add additional info

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ pixkit-image
 ============
 contains image processing related methods which have been published (on articles, e.g., journal or conference papers). 
 In addition, some frequently used related tools are also involved.
+
+Source
+------
+https://github.com/yunfuliu/pixkit-image
+
+License
+-------
+Please read LICENSE.txt in this directory. 
+
--- pixkit-image/pixkit-image.h
+++ pixkit-image/pixkit-image.h
@@ -1,6 +1,6 @@
 //////////////////////////////////////////////////////////////////////////
 // 
-// SOURCE CODE: 
+// SOURCE CODE: https://github.com/yunfuliu/pixkit-image
 // 
 // BEIRF: pixkit-image contains image processing related methods which have been published (on articles, e.g., journal or conference papers). 
 //	In addition, some frequently used related tools are also involved.
