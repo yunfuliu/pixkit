b8d7d9880416a25b08c9ec38d162bf6d72bbf65a
yunfuliu <yunfuliu@gmail.com>
2013-12-27 09:33:12 +0800

    add license info

--- pixkit-image/src/NADD2013/NADD.cpp
+++ pixkit-image/src/NADD2013/NADD.cpp
@@ -1,3 +1,42 @@
+//========================================================================
+//
+// NADD.cpp
+// Authors: Yun-Fu Liu (1), Jing-Ming Guo (2)
+// Institutions: National Taiwan University of Science and Technology
+// Date: Dec. 27, 2013
+// Email: yunfuliu@gmail.com
+// Paper: Yun-Fu Liu and Jing-Ming Guo, "New class tiling design for 
+//        dot-diffused halftoning," IEEE Trans. Image Processing, 
+//        vol. 22, no. 3, pp. 1199-1208, March 2013.
+//
+// NADD Halftoning Copyright (c) 2013, Yun-Fu Liu, all rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification, 
+// are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice, 
+//   this list of conditions and the following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+// 
+// * The name of the copyright holders may not be used to endorse or promote products
+//   derived from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+// IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED 
+// AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF 
+// THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//========================================================================
+
 #include <ctime>
 #include "../../../pixkit-image/pixkit-image.hpp"
 
