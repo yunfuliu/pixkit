795dcb071e8a0a9f59409463e8be1d70f7e93c0a
ocean6204 <yf6204220@hotmail.com>
2014-01-06 18:43:48 +0800

    verify the destinatio vector size and sample project

--- examples-ml/clustering/fuzzyCMeans/example_fuzzyCMeans.cpp
+++ examples-ml/clustering/fuzzyCMeans/example_fuzzyCMeans.cpp
@@ -10,23 +10,26 @@
 
 void main(){
 
-
-
+	// sample point definition (2d case)
 	std::vector<std::vector<double>>	src(6,std::vector<double>(2,0)),dst;
-	src[0][0]=1;	src[0][1]=1;
-	src[1][0]=1;	src[1][1]=2;
-	src[2][0]=2;	src[2][1]=1;
-	src[3][0]=1+5;	src[3][1]=1+5;
-	src[4][0]=1+5;	src[4][1]=2+5;
-	src[5][0]=2+5;	src[5][1]=1+5;
-
-	pixkit::clustering::fuzzyCMeans(src,dst,0,2,2.0,1);
-
-
-
+	src[0][0] = 1;	src[0][1] = 1;
+	src[1][0] = 1;	src[1][1] = 2;
+	src[2][0] = 2;	src[2][1] = 1;
+	src[3][0] = 10;	src[3][1] = 3;
+	src[4][0] = 10;	src[4][1] = 5;
+	src[5][0] = 12;	src[5][1] = 4;
 	
+	// unsupervised clustering with fuzzy c means 
+	pixkit::clustering::fuzzyCMeans(src,dst,1,2,2.0,15);
 
+	// output display
+	for(int i = 0; i < src.size(); i++){
+		for(int j = 0; j < src[0].size(); j++){
+			std::cout << dst[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
 	std::cout		<<	std::endl;
 
-
+	system("pause");
 }
