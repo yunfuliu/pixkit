a7f33f76c8df49beda7207425e0e95fa1db94ef4
yunfuliu <yunfuliu@gmail.com>
2014-08-20 17:45:49 +0800

    change pixkit-file and update to 0.5.1

--- modules/pixkit-file/include/pixkit-file.hpp
+++ modules/pixkit-file/include/pixkit-file.hpp
@@ -1,21 +1,11 @@
 // system related headers
-#include <iostream>
 #include <vector>
-#include <fstream>
 
 #ifndef __PIXKIT_FILE_HPP__
 #define __PIXKIT_FILE_HPP__
-
-typedef const std::string	CStr;
-typedef std::vector<std::string>	vecS;
-
-vecS loadStrList(CStr &fName){
-	std::ifstream fIn(fName);
-	std::string line;
-	vecS strs;
-	while(getline(fIn, line) && line.size())
-		strs.push_back(line);
-	return strs;
+namespace pixkit{
+	typedef const std::string	CStr;
+	typedef std::vector<std::string>	vecS;
+	vecS loadStrList(CStr &fName);
 }
-
 #endif
